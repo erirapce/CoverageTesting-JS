337a88b6ce0e689220f7d945fe60c21130f09134
erirapce <s0566187@htw-berlin.de>
2020-12-16 01:50:20 +0100

    add more comments

--- test/gildedRose.test.js
+++ test/gildedRose.test.js
@@ -1,15 +1,15 @@
 const {Shop, Item} = require("../lib/gildedRose");
 
-const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 3, 8),			//first Item
-									new Item("Love", 0, 0),																				//second Item
-									new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0),	//third Item
-									new Item("Love", 0, 2),																				//fourth Item
-									new Item("Sulfuras, Hand of Ragnaros", -1, 0),								//fifth Item
-									new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-									new Item("Backstage passes to a TAFKAL80ETC concert", 11, 49),
-									new Item("Backstage passes to a TAFKAL80ETC concert", 11, 50),
-									new Item("Aged Brie", 0, 2),
-									new Item("Aged Brie", 0, 49)
+const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 3, 8),//first Item
+									new Item("Love", 0, 0),//second Item
+									new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0),//third Item
+									new Item("Love", 0, 2),//fourth Item
+									new Item("Sulfuras, Hand of Ragnaros", -1, 0),//fifth Item
+									new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),//sixth Item
+									new Item("Backstage passes to a TAFKAL80ETC concert", 11, 49),//seventh Item
+									new Item("Backstage passes to a TAFKAL80ETC concert", 11, 50),//eighth Item
+									new Item("Aged Brie", 0, 2),//ninth Item
+									new Item("Aged Brie", 0, 49)//tenth Item
 								]);
 
 const items = gildedRose.updateQuality();
@@ -25,38 +25,38 @@ describe("a group of tests", () => {
 */
 
 describe.each([
-	[items[0].sellIn, 3],		//first Item
+	[items[0].sellIn, 3],//first Item
 	[items[0].quality, 8],
 
-	[items[1].sellIn, -1],	//second Item
+	[items[1].sellIn, -1],//second Item
 	[items[1].quality, 0],
 
-	[items[2].sellIn, -1],	//third Item
+	[items[2].sellIn, -1],//third Item
 	[items[2].quality, 0],
 
-	[items[3].sellIn, -1],	//fourth Item
+	[items[3].sellIn, -1],//fourth Item
 	[items[3].quality, 0],
 
-	[items[4].sellIn, -1],	//fifth Item
+	[items[4].sellIn, -1],//fifth Item
 	[items[4].quality, 0],
 
-	[items[5].sellIn, 4],
+	[items[5].sellIn, 4],//sixth Item
 	[items[5].quality, 50],
 
-	[items[6].sellIn, 10],
+	[items[6].sellIn, 10],//seventh Item
 	[items[6].quality, 50],
 
-	[items[7].sellIn, 10],
+	[items[7].sellIn, 10],//eighth Item
 	[items[7].quality, 50],
 
-	[items[8].sellIn, -1],
+	[items[8].sellIn, -1],//ninth Item
 	[items[8].quality, 4],
 
-	[items[9].sellIn, -1],
+	[items[9].sellIn, -1],//tenth Item
 	[items[9].quality, 50]
 ])(".updateQuality", (a, expected) => {
 
-	test("Sulfuras, Hand of Ragnaros. sellIn", () => {														//first Item
+	test("Sulfuras, Hand of Ragnaros. sellIn", () => {//first Item
 		expect(a).toBe(expected);
 	});
 
@@ -64,7 +64,7 @@ describe.each([
 		expect(a).toBe(expected);
 	});
 
-	test("Love. sellIn", () => {																									//second Item
+	test("Love. sellIn", () => {//second Item
 		expect(a).toBe(expected);
 	});
 
@@ -72,7 +72,7 @@ describe.each([
 		expect(a).toBe(expected);
 	});
 
-	test("Backstage passes to a TAFKAL80ETC concert. sellIn", () => {							//third Item
+	test("Backstage passes to a TAFKAL80ETC concert. sellIn", () => {//third Item
 		expect(a).toBe(expected);
 	});
 
@@ -80,7 +80,7 @@ describe.each([
 		expect(a).toBe(expected);
 	});
 
-	test("Love. sellIn", () => {																									//fourth Item
+	test("Love. sellIn", () => {//fourth Item
 		expect(a).toBe(expected);
 	});
 
@@ -88,7 +88,7 @@ describe.each([
 		expect(a).toBe(expected);
 	});
 
-	test("Sulfuras, Hand of Ragnaros. sellIn", () => {														//fifth Item
+	test("Sulfuras, Hand of Ragnaros. sellIn", () => {//fifth Item
 		expect(a).toBe(expected);
 	});
 
@@ -96,7 +96,7 @@ describe.each([
 		expect(a).toBe(expected);
 	});
 
-	test("Backstage passes to a TAFKAL80ETC concert. sellIn", () => {							//sixth Item
+	test("Backstage passes to a TAFKAL80ETC concert. sellIn", () => {//sixth Item
 		expect(a).toBe(expected);
 	});
 
@@ -104,7 +104,7 @@ describe.each([
 		expect(a).toBe(expected);
 	});
 
-	test("Backstage passes to a TAFKAL80ETC concert. sellIn", () => {							//seventh Item
+	test("Backstage passes to a TAFKAL80ETC concert. sellIn", () => {//seventh Item
 		expect(a).toBe(expected);
 	});
 
@@ -112,7 +112,7 @@ describe.each([
 		expect(a).toBe(expected);
 	});
 
-	test("Backstage passes to a TAFKAL80ETC concert. sellIn", () => {							//eight Item
+	test("Backstage passes to a TAFKAL80ETC concert. sellIn", () => {//eight Item
 		expect(a).toBe(expected);
 	});
 
@@ -120,7 +120,7 @@ describe.each([
 		expect(a).toBe(expected);
 	});
 
-	test("Aged Brie. sellIn", () => {
+	test("Aged Brie. sellIn", () => {//ninth Item
 		expect(a).toBe(expected);
 	});
 
@@ -128,7 +128,7 @@ describe.each([
 		expect(a).toBe(expected);
 	});
 
-	test("Aged Brie. sellIn", () => {
+	test("Aged Brie. sellIn", () => {//tenth Item
 		expect(a).toBe(expected);
 	});
 
